e51df02947e84b357ae8892fac0ed03bda808587
satsuki-dev <tubillaral@gmail.com>
2024-12-11 10:44:04 +0800

    Booking Pages

--- app/View/Components/IR-sidebar.php
+++ app/View/Components/IR-sidebar.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class IR-sidebar extends Component
+class IRsidebar extends Component
 {
     /**
      * Create a new component instance.

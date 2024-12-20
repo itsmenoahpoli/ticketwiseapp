3d706a17c022b0d25d26c69e23989fbf8f7a9d28
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2024-12-20 11:32:45 +0800

    fix params helpers

--- app/Traits/ParamsHelper.php
+++ app/Traits/ParamsHelper.php
@@ -7,7 +7,7 @@ trait ParamsHelper
     public function createPaginationParams($query)
     {
         return (object) [
-            'pageSize'  => isset($query['pageSize']) ? intval($query['pageSize']) : 1,
+            'pageSize'  => isset($query['pageSize']) ? intval($query['pageSize']) : 25,
             'orderBy'   => isset($query['orderBy']) ? $query['orderBy'] : 'id',
             'sortBy'    => isset($query['sortBy']) ? $query['sortBy'] : 'desc',
         ];
